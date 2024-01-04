067947b868dabeb4fb05d60f2180ea8ff77f1455
sgurra2 <sgurra2@ilstu.edu>
2024-01-04 00:41:02 -0600

    lab

---
+++ write.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    FILE *file;
+    char *filename = "./shashaank_foo";
+    /* open the file for writing */
+    file = fopen(filename, "w");
+    if (file == NULL)
+    {
+        fprintf(stderr, "File %s could not be opened\n", filename);
+        exit(1);
+    }
+    /* write to the file */
+    fprintf(file, "Hello World!\n");
+    /* close the file */
+    fclose(file);
+}
